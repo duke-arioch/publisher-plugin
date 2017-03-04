f909439991cf9c61dbe23856231795142d6a65a2
Matthew Sandoz <matthew.sandoz@gmail.com>
2017-03-03 23:08:45 -0500

    added crypto to the plugin

--- src/main/java/org/sandcast/publisher/plugin/PublisherPlugin.java
+++ src/main/java/org/sandcast/publisher/plugin/PublisherPlugin.java
@@ -8,6 +8,7 @@ import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jasypt.util.text.BasicTextEncryptor;
 import org.sandcast.publisher.plugin.command.PublisherCommand;
 import org.sandcast.publisher.plugin.adapter.DiscordAdapter;
 import org.sandcast.publisher.plugin.adapter.PublishingAdapter;
@@ -16,6 +17,7 @@ public class PublisherPlugin extends JavaPlugin {
 
     private PublishingAdapter adapter;
     private PublishableEventListener publishableListener;
+    private final BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
 
     @Override
     public void onDisable() {
@@ -25,14 +27,17 @@ public class PublisherPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         getConfig().addDefault("discord-token", "DISCORD-TOKEN");
+        final String pluginToken = getConfig().getString("discord-token");
+        getConfig().addDefault("encryption-key", "iguessthisworks?");
         getConfig().options().copyDefaults(true);
         saveConfig();
-        final String pluginToken = getConfig().getString("discord-token");
+        final String encryptionKey = getConfig().getString("discord-token");
+        textEncryptor.setPassword(encryptionKey);
         adapter = new DiscordAdapter(pluginToken); //could instead register multiple including httpaddapter...
         publishableListener = new PublishableEventListener(adapter);
         PluginManager pm = getServer().getPluginManager();
         pm.registerEvents(publishableListener, this);
-        getCommand("discord").setExecutor(new PublisherCommand(adapter));
+        getCommand("discord").setExecutor(new PublisherCommand(adapter, textEncryptor));
         PluginDescriptionFile pdfFile = this.getDescription();
         getLogger().log(Level.INFO, "{0} version {1} is enabled!", new Object[]{pdfFile.getName(), pdfFile.getVersion()});
     }
@@ -40,7 +45,7 @@ public class PublisherPlugin extends JavaPlugin {
     public PublishingAdapter getWireController() {
         return adapter;
     }
-    
+
     public Set<String> publisherTargets() {
         return adapter.publisherTargets();
     }
--- src/main/java/org/sandcast/publisher/plugin/command/PublisherCommand.java
+++ src/main/java/org/sandcast/publisher/plugin/command/PublisherCommand.java
@@ -1,35 +1,52 @@
 package org.sandcast.publisher.plugin.command;
 
 import java.util.Arrays;
+import java.util.Base64;
+import java.util.Base64.Encoder;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+import org.jasypt.util.text.BasicTextEncryptor;
 import org.sandcast.publisher.plugin.adapter.PublishingAdapter;
 
 public class PublisherCommand implements CommandExecutor {
 
     private final PublishingAdapter adapter;
+    BasicTextEncryptor textEncryptor;
     ConsoleCommandSender console;
+    Encoder encoder = Base64.getEncoder().withoutPadding();
 
-    public PublisherCommand(PublishingAdapter adapter) {
+    public PublisherCommand(PublishingAdapter adapter, BasicTextEncryptor textEncryptor) {
         this.adapter = adapter;
+        this.textEncryptor = textEncryptor;
         console = Bukkit.getServer().getConsoleSender();
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] split) {
+        final String subcommand = split[0];
         if (!(sender instanceof Player) && split.length > 1) {
-            final String subcommand = split[0];
-            final String name = split[1];
+            final String name = textEncryptor.decrypt(new String(Base64.getDecoder().decode(split[1])));
+            console.sendMessage("decrypted " + split[1] + " to " + name);
             switch (subcommand.toLowerCase()) {
                 case "clear":
                     adapter.unregister(name);
                     break;
                 case "set":
                     adapter.register(name, Arrays.copyOfRange(split, 2, split.length));
+                    break;
+            }
+        } else {
+            switch (subcommand.toLowerCase()) {
+                case "user":
+                    Player player = (Player) sender;
+                    String encrypted = encoder.encodeToString(textEncryptor.encrypt(player.getName()).getBytes());
+                    player.sendMessage("Your encrypted key is " + ChatColor.YELLOW + "" + ChatColor.BOLD + encrypted);
+                    console.sendMessage("encrypted " + player.getName() + " to " + encrypted);
                     break;
             }
         }
